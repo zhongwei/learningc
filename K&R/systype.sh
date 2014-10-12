386261d4be80d54a48e17286b3c98ae42de254b9
Wei Zhong <zhongwei99@163.com>
2014-10-12 23:05:10 +0800

    Add init Makefile.

---
+++ K&R/systype.sh
@@ -0,0 +1,20 @@
+#
+# Determine the type of *ix operating system that we're
+# running on, and echo an appropriate value.
+# This script is intended to be used in Makefiles.
+# (This is a kludge.  Gotta be a better way.)
+#
+
+case `uname -s` in
+"Linux")
+	PLATFORM="linux"
+	;;
+"Darwin")
+	PLATFORM="macos"
+	;;
+*)
+	echo "Unknown platform" >&2
+	exit 1
+esac
+echo $PLATFORM
+exit 0
