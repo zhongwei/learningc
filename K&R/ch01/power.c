44c4ca16bc473d1c5da2e291f6a5f0b7f2ca8e14
Wei Zhong <zhongwei99@163.com>
2014-11-04 22:37:07 +0800

    Add source code of chapter 1.

---
+++ K&R/ch01/power.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int power(int m, int n);
+
+main() {
+
+	for (int i = 0; i < 10; ++i) {
+		printf("%d %d %d\n", i, power(2, i), power(-3, i));
+	}
+
+}
+
+int power(int base, int n) {
+
+	int		p = 1;
+
+	for (int i = 1; i <= n; ++i) {
+		p = p * base;
+	}
+
+	return p;
+
+}
