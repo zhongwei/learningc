633a8799cce0ccbb70e99b604682fbef43c88703
Wei Zhong <zhongwei99@163.com>
2015-01-01 13:51:07 +0800

    Add codes of chapter 9.

---
+++ cprimerplus/ch09/loccheck.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void mikado(int);		/* declare function */
+
+int main(void)
+{
+	int 		pooh = 2, bah = 5;	/* local to main() */
+
+	printf("In main(), pooh = %d and &pooh = %p\n",
+		pooh, &pooh);
+	printf("In main(), bah = %d and &bah = %p\n",
+		bah, &bah);
+
+	mikado(pooh);
+
+	return 0;
+}
+
+void mikado(int bah)		/* define function */
+{
+	int		pooh = 10;		/* local to mikado() */
+
+	printf("In mikado(), pooh = %d and &pooh = %p\n",
+		pooh, &pooh);
+	printf("in mikado(), bah = %d and &bah = %p\n",
+		bah, &bah);
+}
---
+++ cprimerplus/ch09/swap3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void interchange(int *u, int *v);
+
+int main(void)
+{
+	int		x = 5, y = 10;
+
+	printf("Originally x = %d and y = %d.\n", x, y);
+
+	interchange(&x, &y);		// send addresses to function
+
+	printf("Now x = %d and y = %d.\n", x, y);
+
+	return 0;
+}
+
+void interchange(int *u, int *v)
+{
+	int		temp;
+
+	temp = *u;		// temp gets value that u points to 
+	*u = *v;
+	*v = temp;
+}
