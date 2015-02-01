9088c1670ef63f01eaf72f89f31f148ed41f4a06
Wei Zhong <zhongwei99@163.com>
2015-02-01 20:14:22 +0800

    Add source code of chapter 12.

---
+++ cprimerplus/ch12/r_drive1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+extern void srand1(unsigned int x);
+extern int rand1(void);
+
+int main(void)
+{
+	int		count;
+	unsigned int	seed;
+
+	printf("Please enter your choice for seed.\n");
+
+	while (scanf("%u", &seed) == 1) {
+		srand1(seed);		/* reset seed */
+
+		for (count = 0; count < 5; count++) {
+			printf("%d\n", rand1());
+		}
+
+		printf("Please enter next seed (q to quit):\n");
+	}
+
+	printf("Done\n");
+
+	return 0;
+}
---
+++ cprimerplus/ch12/rand0.c
@@ -0,0 +1,8 @@
+static unsigned long int	next = 1;	/* the seed */
+
+int rand0(void)
+{
+	/* magic formula to generate pseudorandom number */
+	next = next * 1103515245 + 12345;
+	return (unsigned int) (next/65536) % 32768;
+}
